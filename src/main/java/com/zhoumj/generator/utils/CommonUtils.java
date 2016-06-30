5cc6924901b1760eb33b5e5389997b48ae1e514a
vandyzhou <vandyzhou@users.noreply.github.com>
2016-06-30 12:36:26 +0800

    create commonutils
    制造冲突

---
+++ src/main/java/com/zhoumj/generator/utils/CommonUtils.java
@@ -0,0 +1,19 @@
+package com.zhoumj.generator.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author mengjie.zhou
+ * @version 1.00
+ * @date 六月-30-2016,11:33
+ * @description
+ */
+public class CommonUtils {
+
+    public static String getNow(){
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(date);
+    }
+}
